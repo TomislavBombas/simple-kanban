7e94da2f8212ada95044ecabd41e6a8f9d7844dc
Dejan Krivokapic <dejan.krivokapic@gmail.com>
2024-01-06 23:48:49 +0100

    Small cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
